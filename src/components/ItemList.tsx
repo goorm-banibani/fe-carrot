0cb1532e135c24e8e9a24d1773866729178c5380
leesuyeon <lgh0385hh@gmail.com>
2024-12-24 20:38:54 +0900

    FEAT: Searchbar 컴포넌트 생성

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -6,7 +6,7 @@ const ItemList = () => {
     <div className='flex flex-col gap-3'>
       {Items.map(({id, title, price, location, imageUrl}) => (
         <div key={id} className='bg-slate-100 flex p-2'>
-            <img src={imageUrl} className='px-10 rounded-md' />
+            {/* <img src={imageUrl} className='px-10 rounded-md' /> */}
             <div className='flex flex-col p-5'>
                 <span>{title}</span>
                 <span className='text-sm text-gray-500'>{location}</span>
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 import ItemList from "../../components/ItemList";
 import Nav from "../../components/Nav";
-
+import Searchbar from "../../components/Searchbar";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 function MainPage() {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   return (
     <>
-      <Header />
+    {/* 헤더 */}
+      <Header 
+        isSearchOpen={isSearchOpen}
+        setIsSearchOpen={setIsSearchOpen}
+         />
       <ItemList />
       <Nav />
+
+
+      {/* Search Modal */}
+      {isSearchOpen && (
+        <div className='fixed inset-0 z-100 bg-white'>
+          <div className="flex items-center gap-4 mx-4">
+            <div 
+                onClick={() => setIsSearchOpen(!isSearchOpen)}
+                className=" cursor-pointer">
+              <IoIosArrowBack className="text-2xl" />
+            </div>
+              <div className="w-full p-3">
+              <Searchbar />
+              </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
