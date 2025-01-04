a64c7a7cb49ec9a97fb463e30c6638f188eed3d2
junaanjuna <yellowbyme0503@gmail.com>
2025-01-04 16:08:25 +0900

    Layout.tsx contain

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="w-full p-0 m-0 h-auto flex justify-center">
+      <div className="max-w-[600px] w-full">{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
