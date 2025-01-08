acab8f802399846e78efaed4bba59a909d2e8cd2
leesuyeon <lgh0385hh@gmail.com>
2025-01-08 09:42:53 +0900

    FIX: Routes 충돌 해결

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,11 +1,14 @@
 import { Routes as ReactRouterRoutes, Route, Navigate } from 'react-router-dom';
 
+import LoginPage from './LoginPage';
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 
 function Routes() {
   return (
     <ReactRouterRoutes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<MainPage />} />
       <Route path='/search' element={<SearchPage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
